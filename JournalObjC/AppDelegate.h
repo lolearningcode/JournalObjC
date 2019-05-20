9d4e420390e6d76bc7f0e5b116db00d4c5d63ce4
Cleo "Lo" Howard <lolearningcode@gmail.com>
2019-05-20 14:43:07 -0600

    Initial Commit

---
+++ JournalObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalObjC
+//
+//  Created by Lo Howard on 5/20/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JournalObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalObjC
+//
+//  Created by Lo Howard on 5/20/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
